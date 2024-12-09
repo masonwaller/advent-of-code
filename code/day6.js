63224bc5a8105cfaaa426d5bf49aa0dd938b2a0d
Mason Waller <masonwaller805@gmail.com>
2024-12-09 12:47:42 -0700

    day 6 part 2 complete

--- code/day6.js
+++ code/day6.js
@@ -1,14 +1,13 @@
 const fs = require("fs");
 
-const findGuardPath = async () => {
-  const data = await fs.promises.readFile("data.txt", "utf8");
-  const split = data.split("\n").map((x) => x.split(""));
+const findGuardPath = async (split) => {
   const maxY = split.length - 1;
   const maxX = split[0].length - 1;
 
   let x = 0;
   let y = 0;
   let dir = "up";
+  let exitReason = "end";
 
   split.forEach((line, index) => {
     const gaurdStart = line.indexOf("^");
@@ -19,7 +18,8 @@ const findGuardPath = async () => {
   });
 
   split[y][x] = "X";
-  const visited = [`${x},${y}`];
+  const visited = new Set();
+  visited.add(`${dir},${x},${y}`);
 
   while (0 <= x && x <= maxX && 0 <= y && y <= maxY) {
     if (dir === "up") {
@@ -47,14 +47,49 @@ const findGuardPath = async () => {
         dir = "up";
       }
     }
-    if (split[y]?.[x] !== "X") {
+    if (visited.has(`${dir},${x},${y}`)) {
+      exitReason = "loop";
+      break;
+    } else if (split[y]?.[x] && split[y][x] !== "X") {
       split[y][x] = "X";
-      visited.push(`${x},${y}`);
+      visited.add(`${dir},${x},${y}`);
     }
   }
-
-  console.log(visited.length - 1);
-  return visited.length - 1;
+  return { visited, exitReason };
 };
 
-findGuardPath();
+const copy = (matrix) => matrix.map((row) => row.map((char) => char));
+
+const findLoops = async (matrix, guardFloorPlan) => {
+  let infiniteLoops = 0;
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (
+        matrix[y][x] !== "#" &&
+        matrix[y][x] !== "^" &&
+        guardFloorPlan[y][x] !== "."
+      ) {
+        let matrix2 = copy(matrix);
+        matrix2[y][x] = "#";
+        const { visited, exitReason } = await findGuardPath(matrix2);
+        if (exitReason === "loop") {
+          infiniteLoops++;
+        }
+      }
+    }
+  }
+  return infiniteLoops;
+};
+
+const day6 = async () => {
+  const data = await fs.promises.readFile("data.txt", "utf8");
+  const split = data.split("\n").map((x) => x.split(""));
+  const guardFloorPlan = copy(split);
+  const { visited, exitReason } = await findGuardPath(guardFloorPlan);
+  const loops = await findLoops(copy(split), guardFloorPlan);
+
+  console.log(visited.size, loops);
+  return visited.size;
+};
+
+day6();
